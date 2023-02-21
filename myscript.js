16eb1150929c32e656c111d0f5c09555bcad98dd
Dr. Raju Ranjan Kumar Paswan <rrpaswan@gmail.com>
2023-02-21 22:56:10 +0000

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -16,3 +16,21 @@ async function predictSentiment(text) {
 // Call the function with a piece of text
 const result = await predictSentiment('I hate this product. It is terrible.');
 console.log(result.toxicityPrediction); // outputs true
+const functions = require('firebase-functions');
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+
+  function handleWelcomeIntent(agent) {
+    agent.add('Hello! How can I assist you?');
+  }
+
+  // Define other intent handlers here
+
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', handleWelcomeIntent);
+  // Add other intents to the intent map here
+
+  agent.handleRequest(intentMap);
+});
