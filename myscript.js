aa2b3405e6f16543a1933a1db99f5b413b3ee476
Dr. Raju Ranjan Kumar Paswan <rrpaswan@gmail.com>
2023-02-21 22:53:10 +0000

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,18 @@
 
+// Load the TensorFlow.js library
+import * as tf from '@tensorflow/tfjs';
+import * as toxicity from '@tensorflow-models/toxicity';
+
+// Load the model and specify the labels to check for
+const model = await toxicity.load(0.9, ['toxicity']);
+
+// Define a function to perform sentiment analysis
+async function predictSentiment(text) {
+  const predictions = await model.classify(text);
+  const toxicityPrediction = predictions[0].results[0].match;
+  return { text, toxicityPrediction };
+}
+
+// Call the function with a piece of text
+const result = await predictSentiment('I hate this product. It is terrible.');
+console.log(result.toxicityPrediction); // outputs true
